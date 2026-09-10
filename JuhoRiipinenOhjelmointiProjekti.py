01fb0a48dfb25bde87b9ccc112a91fb1fccbaafb
Juho Riipinen <juho.riipinen@ICTLAB.local>
2026-09-10 13:09:51 +0300

    first commit

---
+++ JuhoRiipinenOhjelmointiProjekti.py
@@ -0,0 +1,44 @@
+#Ohjelmointiprojekti - Juho Riipinen
+
+#Osion 1 funktio
+def Osio1():
+  print("-- Osio 1 --")
+
+#Osion 2 funktio
+#def Osio2():
+#  print("-- Osio 2 --")
+
+#Osion 3 funktio
+#def Osio3():
+#  print("-- Osio 3 --")
+
+#Osion 4 funktio
+#def Osio4():
+#  print("-- Osio 4 --")
+
+#Osion 5 funktio
+#def Osio5():
+#  print("-- Osio 5 --")
+
+#Kirjoita loppuosioiden funktiot samalla kaavalla...
+
+#Ylle kirjoitettujen funktioiden kutsut ja kommentti siitä mitä opit kyseisessä osiossa:
+
+#Tässä osiossa käsiteltiin esiteltiin muuttujat, input ja output.
+#Muuttujat ja niiden tyypit jäivät vielä hieman epäselviksi,
+#mutta niitä käydään läpi seuraavalla kerralla tarkemmin.
+#Ymmärsin kuitenkin miten käyttäjän antamaa syötettä
+#voidaan käyttää ohjelmassa esim. laskutoimituksien suorittamiseen. 
+Osio1()
+
+#Kommentoi osiossa käsiteltyjä asioita ja kerro mitä opit osiossa ja/tai mikä jäi epäselväksi.
+#Osio2()
+
+#Kommentoi osiossa käsiteltyjä asioita ja kerro mitä opit osiossa ja/tai mikä jäi epäselväksi.
+#Osio3()
+
+#Kommentoi osiossa käsiteltyjä asioita ja kerro mitä opit osiossa ja/tai mikä jäi epäselväksi.
+#Osio4()
+
+#Kommentoi osiossa käsiteltyjä asioita ja kerro mitä opit osiossa ja/tai mikä jäi epäselväksi.
+#Osio5()
